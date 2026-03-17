a672d96382b963207945ddffd32b2d7346339c91
qwerltaz <69696513+qwerltaz@users.noreply.github.com>
2026-03-17 13:38:09 +0100

    expand exact and substring match tests

--- tests/test_maps.py
+++ tests/test_maps.py
@@ -15,17 +15,35 @@ class TestMaps(unittest.TestCase):
     def test_map_exact_match(self):
         map_exact_match = lex2spdx.maps.MapExactMatch()
 
+        # Title text
         self.assertEqual("MIT", map_exact_match.map("MIT License"))
         self.assertIsNone(map_exact_match.map("MIT License "))
         self.assertIsNone(map_exact_match.map("MIT  License"))
         self.assertIsNone(map_exact_match.map("mit  License"))
+        # Name
+        self.assertEqual("Unlicense", map_exact_match.map("The Unlicense"))
+        # Text
         self.assertEqual("LGPL-2.0", map_exact_match.map("GNU LIBRARY GENERAL PUBLIC LICENSE Version 2, June 1991"))
+        self.assertEqual("MIPS", map_exact_match.map(
+            "Copyright (c) 1992, 1991, 1990 MIPS Computer Systems, Inc. MIPS Computer Systems, Inc. grants "
+            "reproduction and use rights to all parties, PROVIDED that this comment is maintained in the copy."))
 
     def test_map_substring(self):
         map_substring = lex2spdx.maps.MapSubstring()
 
+        # Title text
         self.assertEqual("MIT", map_substring.map("MIT License"))
         self.assertEqual("MIT", map_substring.map(" MIT License "))
+        # Name
+        self.assertEqual("Unlicense", map_substring.map("The Unlicense"))
+        # Text
+        self.assertEqual("MIPS", map_substring.map(
+            "Copyright (c) 1992, 1991, 1990 MIPS Computer Systems, Inc. MIPS Computer Systems, Inc. grants "
+            "reproduction and use rights to all parties, PROVIDED that this comment is maintained in the copy."))
+        self.assertEqual("MIPS", map_substring.map(
+            "lipsum Copyright (c) 1992, 1991, 1990 MIPS Computer Systems, Inc. MIPS Computer Systems, Inc. grants "
+            "reproduction and use rights to all parties, PROVIDED that this comment is maintained in the copy. lipsum"))
+
         self.assertIsNone(map_substring.map("MIT  License"))
 
 
