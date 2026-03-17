1783c4b10fd0f0bd11536de29e3924072e57f977
qwerltaz <69696513+qwerltaz@users.noreply.github.com>
2026-03-17 13:26:37 +0100

    exact ID and substring map tests

--- tests/test_maps.py
+++ tests/test_maps.py
@@ -4,6 +4,14 @@ import lex2spdx.maps
 
 
 class TestMaps(unittest.TestCase):
+    def test_map_exact_ID(self):
+        map_exact_id = lex2spdx.maps.MapExactID()
+
+        self.assertEqual("MIT", map_exact_id.map("MIT"))
+        self.assertIsNone(map_exact_id.map("MIT License"))
+        self.assertIsNone(map_exact_id.map(""))
+        self.assertIsNone(map_exact_id.map("GNU LIBRARY GENERAL PUBLIC LICENSE Version 2, June 1991"))
+
     def test_map_exact_match(self):
         map_exact_match = lex2spdx.maps.MapExactMatch()
 
@@ -12,6 +20,13 @@ class TestMaps(unittest.TestCase):
         self.assertIsNone(map_exact_match.map("MIT  License"))
         self.assertIsNone(map_exact_match.map("mit  License"))
         self.assertEqual("LGPL-2.0", map_exact_match.map("GNU LIBRARY GENERAL PUBLIC LICENSE Version 2, June 1991"))
+
+    def test_map_substring(self):
+        map_substring = lex2spdx.maps.MapSubstring()
+
+        self.assertEqual("MIT", map_substring.map("MIT License"))
+        self.assertEqual("MIT", map_substring.map(" MIT License "))
+        self.assertIsNone(map_substring.map("MIT  License"))
 
 
 if __name__ == '__main__':
