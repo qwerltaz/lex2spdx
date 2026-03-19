7fa3b099d2a0ffeae0ff0f48df3359681c0688c1
qwerltaz <69696513+qwerltaz@users.noreply.github.com>
2026-03-19 14:00:29 +0100

    convert to pytest

--- tests/test_maps.py
+++ tests/test_maps.py
@@ -1,51 +1,46 @@
-import unittest
-
 import lex2spdx.maps
 
 
-class TestMaps(unittest.TestCase):
-    def test_map_exact_ID(self):
-        map_exact_id = lex2spdx.maps.MapExactID()
+def test_map_exact_id():
+    map_exact_id = lex2spdx.maps.MapExactID()
 
-        self.assertEqual("MIT", map_exact_id.map("MIT"))
-        self.assertIsNone(map_exact_id.map("MIT License"))
-        self.assertIsNone(map_exact_id.map(""))
-        self.assertIsNone(map_exact_id.map("GNU LIBRARY GENERAL PUBLIC LICENSE Version 2, June 1991"))
-
-    def test_map_exact_match(self):
-        map_exact_match = lex2spdx.maps.MapExactMatch()
-
-        # Title text
-        self.assertEqual("MIT", map_exact_match.map("MIT License"))
-        self.assertIsNone(map_exact_match.map("MIT License "))
-        self.assertIsNone(map_exact_match.map("MIT  License"))
-        self.assertIsNone(map_exact_match.map("mit  License"))
-        # Name
-        self.assertEqual("Unlicense", map_exact_match.map("The Unlicense"))
-        # Text
-        self.assertEqual("LGPL-2.0", map_exact_match.map("GNU LIBRARY GENERAL PUBLIC LICENSE Version 2, June 1991"))
-        self.assertEqual("MIPS", map_exact_match.map(
-            "Copyright (c) 1992, 1991, 1990 MIPS Computer Systems, Inc. MIPS Computer Systems, Inc. grants "
-            "reproduction and use rights to all parties, PROVIDED that this comment is maintained in the copy."))
-
-    def test_map_substring(self):
-        map_substring = lex2spdx.maps.MapSubstring()
-
-        # Title text
-        self.assertEqual("MIT", map_substring.map("MIT License"))
-        self.assertEqual("MIT", map_substring.map(" MIT License "))
-        # Name
-        self.assertEqual("Unlicense", map_substring.map("The Unlicense"))
-        # Text
-        self.assertEqual("MIPS", map_substring.map(
-            "Copyright (c) 1992, 1991, 1990 MIPS Computer Systems, Inc. MIPS Computer Systems, Inc. grants "
-            "reproduction and use rights to all parties, PROVIDED that this comment is maintained in the copy."))
-        self.assertEqual("MIPS", map_substring.map(
-            "lipsum Copyright (c) 1992, 1991, 1990 MIPS Computer Systems, Inc. MIPS Computer Systems, Inc. grants "
-            "reproduction and use rights to all parties, PROVIDED that this comment is maintained in the copy. lipsum"))
-
-        self.assertIsNone(map_substring.map("MIT  License"))
+    assert map_exact_id.map("MIT") == "MIT"
+    assert map_exact_id.map("MIT License") is None
+    assert map_exact_id.map("") is None
+    assert map_exact_id.map("GNU LIBRARY GENERAL PUBLIC LICENSE Version 2, June 1991") is None
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_map_exact_match():
+    map_exact_match = lex2spdx.maps.MapExactMatch()
+
+    # Title text
+    assert map_exact_match.map("MIT License") == "MIT"
+    assert map_exact_match.map("MIT License ") is None
+    assert map_exact_match.map("MIT  License") is None
+    assert map_exact_match.map("mit  License") is None
+    # Name
+    assert map_exact_match.map("The Unlicense") == "Unlicense"
+    # Text
+    assert map_exact_match.map("GNU LIBRARY GENERAL PUBLIC LICENSE Version 2, June 1991") == "LGPL-2.0"
+    assert map_exact_match.map(
+        "Copyright (c) 1992, 1991, 1990 MIPS Computer Systems, Inc. MIPS Computer Systems, Inc. grants "
+        "reproduction and use rights to all parties, PROVIDED that this comment is maintained in the copy.") == "MIPS"
+
+
+def test_map_substring():
+    map_substring = lex2spdx.maps.MapSubstring()
+
+    # Title text
+    assert map_substring.map("MIT License") == "MIT"
+    assert map_substring.map(" MIT License ") == "MIT"
+    # Name
+    assert map_substring.map("The Unlicense") == "Unlicense"
+    # Text
+    assert map_substring.map(
+        "Copyright (c) 1992, 1991, 1990 MIPS Computer Systems, Inc. MIPS Computer Systems, Inc. grants "
+        "reproduction and use rights to all parties, PROVIDED that this comment is maintained in the copy.") == "MIPS"
+    assert map_substring.map(
+        "lipsum Copyright (c) 1992, 1991, 1990 MIPS Computer Systems, Inc. MIPS Computer Systems, Inc. grants "
+        "reproduction and use rights to all parties, PROVIDED that this comment is maintained in the copy. lipsum") == "MIPS"
+
+    assert map_substring.map("MIT  License") is None
