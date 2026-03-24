7a9a968d1db1ce609ccf87dc278abe44fda6c652
qwerltaz <69696513+qwerltaz@users.noreply.github.com>
2026-03-24 14:46:35 +0100

    improve rapidfuzz tool

--- tools/fuzz_partial_ratio_test.py
+++ tools/fuzz_partial_ratio_test.py
@@ -1,19 +1,31 @@
 """Print rapidfuzz partial ratio similarity for hardcoded pairs of strings."""
+
 from rapidfuzz.fuzz import partial_ratio
+
+from lex2spdx.load_spdx_licenses import get_licenses
+
+LICENSES = get_licenses()
+
+gpl3_text = LICENSES[4]["text"]
 
 checks: list[tuple[str, str]] = [
     ("License :: OSI Approved :: Apache Software License",
      "The Apache Software License, Version 1.1 Copyright (c) 2000 The Apache Software Foundation. All "
      "rights reserved. Redistribution and use in source and binary forms, with or without "
      "modification, are permitted provide"),
-    ("", ""),
+    ("License :: OSI Approved :: Apache Software License", gpl3_text),
+    ("1234567890", gpl3_text),
+    ("MIT License: http://opensource.org/licenses/MIT", "MIT License"),
+    ("http://opensource.org/licenses/MIT", "MIT License"),
 ]
+
+# for i, l in enumerate(LICENSES):
+#     print(i, l["name"])
 
 for check in checks:
     license_field, ground_truth = check
 
-    print(f"partial ratio for:")
+    print(f"{partial_ratio(*check):.2f} partial ratio for:")
     print(f"{license_field= }")
     print(f"{ground_truth= }")
-    print("is ", partial_ratio(*check))
     print()
