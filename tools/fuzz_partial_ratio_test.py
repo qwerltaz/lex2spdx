b5c675e92a56ea0257259bc89dcdbc8d715d7d2e
qwerltaz <69696513+qwerltaz@users.noreply.github.com>
2026-04-09 13:59:20 +0200

    fuzzy partial ratio test: encapsulate in main function

--- tools/fuzz_partial_ratio_test.py
+++ tools/fuzz_partial_ratio_test.py
@@ -4,23 +4,29 @@ from rapidfuzz.fuzz import partial_ratio
 
 from lex2spdx.spdx_license_data import LicenseData
 
-gpl3_text = LicenseData.licenses[4]["text"]
 
-checks: list[tuple[str, str]] = [
-    ("License :: OSI Approved :: Apache Software License",
-     "The Apache Software License, Version 1.1 Copyright (c) 2000 The Apache Software Foundation. All "
-     "rights reserved. Redistribution and use in source and binary forms, with or without "
-     "modification, are permitted provide"),
-    ("License :: OSI Approved :: Apache Software License", gpl3_text),
-    ("1234567890", gpl3_text),
-    ("MIT License: http://opensource.org/licenses/MIT", "MIT License"),
-    ("http://opensource.org/licenses/MIT", "MIT License"),
-]
+def main():
+    gpl3_text = LicenseData.licenses[4]["text"]
 
-for check in checks:
-    license_field, ground_truth = check
+    checks: list[tuple[str, str]] = [
+        ("License :: OSI Approved :: Apache Software License",
+         "The Apache Software License, Version 1.1 Copyright (c) 2000 The Apache Software Foundation. All "
+         "rights reserved. Redistribution and use in source and binary forms, with or without "
+         "modification, are permitted provide"),
+        ("License :: OSI Approved :: Apache Software License", gpl3_text),
+        ("1234567890", gpl3_text),
+        ("MIT License: http://opensource.org/licenses/MIT", "MIT License"),
+        ("http://opensource.org/licenses/MIT", "MIT License"),
+    ]
 
-    print(f"{partial_ratio(*check):.2f} partial ratio for:")
-    print(f"{license_field= }")
-    print(f"{ground_truth= }")
-    print()
+    for check in checks:
+        license_field, ground_truth = check
+
+        print(f"{partial_ratio(*check):.2f} partial ratio for:")
+        print(f"{license_field= }")
+        print(f"{ground_truth= }")
+        print()
+
+
+if __name__ == '__main__':
+    main()
