1cf59f138dc0717d943a612454dfd3527935dcf2
qwerltaz <69696513+qwerltaz@users.noreply.github.com>
2026-04-29 15:07:01 +0200

    fix unknown license fields json
    leave them as-is in the resource and then apply the same normalization as license fields to prevent error when adding new ones.

--- lex2spdx/maps.py
+++ lex2spdx/maps.py
@@ -5,7 +5,7 @@ from typing import Literal
 
 import rapidfuzz
 
-from . import cvar
+from . import cvar, preprocess
 from . import logger
 from .spdx_license_data import LicenseData, LicenseDataNormalized
 
@@ -43,7 +43,9 @@ class MapNA(IMap):
     def __init__(self):
         super().__init__()
         with open(cvar.resources_dir / "unknown_license_fields.json", "r", encoding="utf-8") as f:
-            self.bad_values: list = json.load(f)
+            self.bad_values: set = set(json.load(f))
+        self.bad_values = set(map(preprocess.normalize_license_field, self.bad_values))
+        print(self.bad_values)
 
     def map(self, license_field: str):
         if license_field.lower() in self.bad_values:
